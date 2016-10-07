bda7e1d1b8e0badc7d1a015e2789b3226d7fe20a
Trent Butler <redtrentbutler@gmail.com>
2016-10-07 17:52:55 -0500

    Text Based 10-07-16

--- TBAG Game/Grid.h
+++ TBAG Game/Grid.h
@@ -10,17 +10,22 @@ class Grid
 {
 
 public:
-	Grid()
-	{
-
-	}
+	Grid() {};
+	
 	Grid(bool n, bool s, bool e, bool w, const char* description) 
 	{
 		p_description = description;
+		g_north = n;
+		g_south = s;
+		g_east = e;
+		g_west = w;
 	}
 	
 	const char* p_description;
-
+	bool g_north;
+	bool g_south;
+	bool g_east;
+	bool g_west;
 	
 
 };
--- TBAG Game/Main.cpp
+++ TBAG Game/Main.cpp
@@ -15,47 +15,42 @@ class Player
 public:
 	Player(Grid &room) :p_index(0), currentRoom(&room)
 	{
-		cout << currentRoom->p_description;
+		/*cout << currentRoom->p_description;*/
 	}
+
 	Player() {};
+		
+	
 	Grid * currentRoom;
+	
 	int p_index;
 
 	void moveRight()
 	{
-	
-	if (p_index < 16)
-	{
-		cout << currentRoom->p_description << "\n";
-		currentRoom++;
-	}
 
-	else
-	{
-		
-		cout << " \n Nope!!!! \n";
-	}
+		if (currentRoom->g_east == true)
+		{
+			cout << currentRoom->p_description << "\n";
+			currentRoom++;
+		}
+
 	}
 
 	void moveLeft()
 	{
-		if (p_index > 16)
+		if (currentRoom->g_west == true)
 		{
 			cout << currentRoom->p_description << "\n";
 			currentRoom--;
 		}
-
-		else
-		{
-			cout << "\n Nope!!!! \n";
-		}
 	}
 
-	void moveDown()
+	void moveDown() 
 	{
-		p_index += 4;
 
-		if (p_index < 16)
+		p_index = 4;
+
+		if (p_index < 16 && currentRoom->g_south == true)
 		{
 			currentRoom += p_index;
 			cout << currentRoom->p_description;
@@ -65,39 +60,35 @@ public:
 			cout << currentRoom->p_description;
 			cout << "NOPE!";
 		}
+
+
+
 	}
 	
-	void moveUp()
+	void moveUp() 
 	{
 
-		p_index -= 4;
-		if (p_index < 0)
+		p_index = 4;
+		if (p_index > 0 && currentRoom->g_north == true)
 		{
 			currentRoom -= p_index;
 			cout << currentRoom->p_description;
 		}
+
+		else
+		{
+			currentRoom->p_description;
+			cout << "Nope!!!! \n";
+		}
+		
+
 	}
 
 
 	bool p_item;
-
+	
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -106,89 +97,194 @@ int main()
 {
 
 
-	
-	bool runTBAG = true;
+
+	bool runTBAG = false;
 	int x = 0;
 	int y = 0;
 	int count = 0;
 
-	Grid room1 = Grid{ 1,0,1,0,"This room is empty \n You are in room 1.... \n" };
-	Grid room2 = Grid{ 1,0,1,1,"This room is empty \n You are in room 2.... \n" };
-	Grid room3 = Grid{ 1,0,1,1,"This room is empty \n You are in room 3.... \n" };
-	Grid room4 = Grid{ 1, 0, 0, 1,"This room is empty \n You are in room 4.... \n" };
+	Grid room0 = Grid(0, 1, 1, 1, "Game Over \nYou Win! \n \nPress 'Q' to quit.... \n \n");
+	Grid room1 = Grid(0, 1, 1, 1, "This room is empty \n Entering Room 1");
+	Grid room2 = Grid(0, 1, 1, 1, "This room is empty \n Entering Room 2");
+	Grid room3 = Grid(0, 1, 0, 1, "This room is empty \n Entering Room 3");
 
-	Grid room5 = Grid{ 1,1,1,0, "This room is empty \n You are in room 5.... \n" };
-	Grid room6 = Grid{ 1,1,1,1, "This room is empty \n You are in room 6.... \n" };
-	Grid room7 = Grid{ 1,1,1,1,"This room is empty \n You are in room 7.... \n" };
-	Grid room8 = Grid{ 1,1,0,1,"This room is empty \n You are in room 8.... \n" };
+	Grid room4 = Grid(1, 1, 0, 0, "This room is empty \n Entering Room 4");
+	Grid room5 = Grid(1, 1, 1, 1, "This room is empty \n Entering Room 5");
+	Grid room6 = Grid(1, 1, 1, 1, "This room is empty \n Entering Room 6");
+	Grid room7 = Grid(1, 1, 0, 1, "This room is empty \n Entering Room 7");
 
-	Grid room9 = Grid{ 1,1,1,0,"This room is empty \n You are in room 9.... \n" };
-	Grid room10 = Grid{ 1,1,1,1,"This room is empty \n You are in room 10.... \n" };
-	Grid room11 = Grid{ 1,1,1,1,"This room is empty \n You are in room 11.... \n" };
-	Grid room12 = Grid{ 1,1,0,1,"This room is empty \n You are in room 12.... \n" };
+	Grid room8 = Grid(1, 1, 0, 0, "This room is empty \n Entering Room 8");
+	Grid room9 = Grid(1, 1, 1, 1, "This room is empty \n Entering Room 9");
+	Grid room10 = Grid(1, 1, 1, 1, "This room is empty \n Entering Room 10");
+	Grid room11 = Grid(1, 1, 0, 1, "This room is empty \n Entering Room 11");
 
-	Grid room13 = Grid{ 0,1,1,0,"This room is empty \n You are in room 13.... \n" };
-	Grid room14 = Grid{ 0,1,1,1,"This room is empty \n You are in room 14.... \n" };
-	Grid room15 = Grid{ 0,1,1,1,"This room is empty \n You are in room 15.... \n" };
-	Grid room16 = Grid{ 0,1,0,1,"This room is empty \n You are in room 16.... \n" };
+	Grid room12 = Grid(1, 0, 0, 1, "This room is empty \n Entering Room 12");
+	Grid room13 = Grid(1, 0, 1, 0, "This room is empty \n Entering Room 13");
+	Grid room14 = Grid(1, 0, 1, 1, "This room is empty \n Entering Room 14");
+	Grid room15 = Grid(1, 0, 1, 1, "This room is empty \n Entering Room 15");
 
 	Grid nodes[16] = {
-		room1,room2,room3,room4,
-		room5,room6,room7,room8,
-		room9,room10,room11,room12,
-		room13,room14,room15,room16
+		room0,room1,room2,room3,
+		room4,room5,room6,room7,
+		room8,room9,room10,room11,
+		room12,room13,room14,room15
 	};
 
-	Player Elliot = Player(nodes[0]);
+
+	Player Elliot = Player(nodes[15]); //  PLAYER
 	
 	
-	while (runTBAG = true)
+	
+	Grid grid = Grid();
+	Player player = Player();
+
+	
+	while (true)
 	{
-		char gameInput;
-		cin >> gameInput;
 		system("cls");
-		switch (gameInput)
+		cout << "\n \n";
+		cout << "Find the exit.... \n \n";
+		cout << "Good Luck...... \n \n";
+		cout << "To display controls enter '?' \n \n";
+		cout << "To start game enter 'S' \n \n";
+		cout << "To exit game enter 'Q' \n \n";
+			
+
+		char startGame;
+		cin >> startGame;
+
+		switch (startGame)
 		{
-		case 'w':
+
+		case 'S': // Start the game....
 		{
-			Elliot.moveUp();
-			break;
-		}
+			runTBAG = true;
+			while (runTBAG == true)
+			{
+						
 				
-		case's':
+				char gameInput;
+				cin >> gameInput;
+				system("cls");
+
+
+				if (player.p_index > 16)
+				{
+											
+					player.p_index = 16;
+					
+				}
+
+				if (player.p_index < 0)
+				{
+					player.p_index = 0;
+				}
+
+				/*if (grid.g_north == false)
+				{
+					
+					cout << "Player cannot go that way...";
+
+				}*/
+
+
+				
+				switch (gameInput)
+				{
+				case 'w':
+				{
+					Elliot.moveUp();
+					
+					break;
+				}
+
+				case 's':
+				{
+					Elliot.moveDown();
+					break;
+				}
+
+				case 'a':
+				{
+					Elliot.moveLeft();
+					break;
+				}
+
+				case 'd':
+				{
+					Elliot.moveRight();
+					break;
+				}
+				
+				case '?':
+				{
+					cout << "Move Up ~~>> w <<~~ \n \n";
+					cout << "Move Down ~~>> s <<~~ \n \n";
+					cout << "Move Left ~~>> a <<~~ \n \n";
+					cout << "Move Right ~~>> d <<~~ \n \n";
+					break;
+				}
+				
+				case 'Q':
+				{
+					cout << "Lata broski! \n";
+					system("pause,");
+					runTBAG = false;
+					break;
+
+				}
+
+				default:
+				{
+					system("cls");
+					cout << "\n \n";
+					cout << "Wrong Input bro";
+					cout << "\n \n";
+					break;
+				}
+
+
+
+				}
+				
+
+
+			}
+			break;
+		}
+
+		case '?':
 		{
-			Elliot.moveDown();
+			cout << "\n \n";
+			cout << "Move Up ~~>> w <<~~ \n \n";
+			cout << "Move Down ~~>> s <<~~ \n \n";
+			cout << "Move Left ~~>> a <<~~ \n \n";
+			cout << "Move Right ~~>> d <<~~ \n \n";
+			cout << "\n \n";
+			system("pause");
 			break;
 		}
 		
-		case'a':
+		case 'Q':
 		{
-			Elliot.moveLeft();
-			break;
+			cout << "\n \n";
+			cout << "Until next time.... \n \n";
+			system("pause");
+			return 0;
 		}
-		
-		case 'd':
-		{
-			Elliot.moveLeft();
-			break;
-		}
-		
-
-		default:
-		{
-			system("cls");
-			cout << "Wrong Input bro";
-			break;
-		}
-		
 
 
 		}
-		
-
 
 		
+
 	}
+
+
+
+
+	
+	
 
 
 
